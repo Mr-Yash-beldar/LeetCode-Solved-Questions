804b5522114dfc5181235885be2d866d04b67b0a
Yashodip Beldar <106594839+Mr-Yash-beldar@users.noreply.github.com>
2024-07-12 16:51:54 +0530

    Time: 30 ms (63.11%), Space: 34.8 MB (50.88%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -17,8 +17,56 @@ public:
         return vector<int>(arr.begin()+low,arr.begin()+high+1);
     }
 
+     int lowerBound(vector<int> arr,int x){
+            int start=0,end=arr.size()-1;
+            int ans=end;
+            while(start<=end){
+                int mid=start+(end-start)/2;
+                if(arr[mid]>=x){
+                    ans=mid;
+                     end=mid-1;
+                    
+                }
+                else if(arr[mid]<x){
+                    start=mid+1;
+                }
+                else{
+                     end=mid-1;
+                }
+            }
+            return ans;
+        }
+
+    vector<int> bs_method(vector<int>& arr, int k, int x) {
+        int high=lowerBound(arr,x);
+        int low=high-1;
+        while(k--){
+            if(low<0){
+                high++;
+            }
+            else if(high>=arr.size())
+                low--;
+            else if(x-arr[low]>arr[high]-x)
+                high++;
+            else
+                low--;
+        }
+          // vector<int> ans;
+        // for(int i=low;i<=high;i++){
+        //     ans.push_back(arr[i]);
+        // }
+        return vector<int>(arr.begin()+low+1,arr.begin()+high);
+    }
+
+
+
+    
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-      return twoPointer(arr,k,x);
+        // cout<<lowerBound(arr,x);
+    //   return twoPointer(arr,k,x);
+      return bs_method(arr,k,x);
+   
+
     }
 
 };
