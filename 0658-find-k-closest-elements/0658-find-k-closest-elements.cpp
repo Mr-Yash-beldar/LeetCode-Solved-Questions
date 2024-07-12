a42125b1d360d91d28e6cbcf8cbe816787b824b6
Yashodip Beldar <106594839+Mr-Yash-beldar@users.noreply.github.com>
2024-07-12 15:48:11 +0530

    Time: 27 ms (75.86%), Space: 33.9 MB (75.4%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -3,18 +3,18 @@ public:
 
     vector<int> twoPointer(vector<int>& arr, int k, int x) {
         int low=0,high=arr.size()-1;
-        vector<int> ans;
+      
         while(high-low>=k){
             if(abs(x-arr[low])>abs(x-arr[high]))
                 low++;
             else
                 high--;
         }
-        
-        for(int i=low;i<=high;i++){
-            ans.push_back(arr[i]);
-        }
-        return ans;
+          // vector<int> ans;
+        // for(int i=low;i<=high;i++){
+        //     ans.push_back(arr[i]);
+        // }
+        return vector<int>(arr.begin()+low,arr.begin()+high+1);
     }
 
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
