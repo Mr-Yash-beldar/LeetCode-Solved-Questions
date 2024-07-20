75d5cd883fa0f3e9d8f6ef990575bca8f310b563
Yashodip Beldar <106594839+Mr-Yash-beldar@users.noreply.github.com>
2024-07-20 14:21:21 +0530

    Time: 3 ms (38.24%), Space: 8.5 MB (6.7%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -12,34 +12,32 @@ public:
         int count=0;
         //starting row
         while(count<totalCount){
-        for(int i=startingRow;i<=endingCol && count<totalCount;i++){
+        for(int i=startingRow;i<=endingCol && count<totalCount;i++){  //go from starting row to ending col
             ans.push_back(matrix[startingRow][i]);
             count++; 
         }
-        startingRow++;
+        startingRow++; //increase starting row
 
         //endingcol
-        for(int i=startingRow;i<=endingRow && count<totalCount;i++ ){
+        for(int i=startingRow;i<=endingRow && count<totalCount;i++ ){ //go starting row to ending row
             ans.push_back(matrix[i][endingCol]);
             count++;
         }
-        endingCol--;
+        endingCol--; //decrease ending col as to shrink matrix go to left
 
         //ending row
-         for(int i=endingCol;i>=startingCol && count<totalCount;i--){
+         for(int i=endingCol;i>=startingCol && count<totalCount;i--){ // go from ending col to starting col
             ans.push_back(matrix[endingRow][i]);
             count++;
         }
-        endingRow--;
+        endingRow--; //decrease ending row as to shrink matrix go up spiral
         
          for(int i=endingRow;i>=startingRow && count<totalCount;i--){
             ans.push_back(matrix[i][startingCol]);
             count++;
         }
-        startingCol++;
-        if(count==totalCount){
-            return ans;
-        }
+        startingCol++; //increase string col go to right shrink it spiral
+
         }
         return ans;
 
