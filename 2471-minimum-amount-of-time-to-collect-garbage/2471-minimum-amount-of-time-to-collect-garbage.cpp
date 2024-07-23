99c0999e5a3367e617b12b3f89f1d6aae575978a
Yashodip Beldar <106594839+Mr-Yash-beldar@users.noreply.github.com>
2024-07-24 01:56:03 +0530

    Time: 154 ms (67.32%), Space: 106.1 MB (23.93%) - LeetHub

---
+++ 2471-minimum-amount-of-time-to-collect-garbage/2471-minimum-amount-of-time-to-collect-garbage.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+        int totalTime = 0;
+        int pickP = 0; //plastic pick time
+        int pickG = 0; //glass pick time
+        int pickM = 0; //metal pick time
+
+        int lastP = -1; //last occurrence of plastic
+        int lastG = -1; //last occurrence of glass
+        int lastM = -1; //last occurrence of metal
+
+        // Calculate the total picking time and the last occurrence of each garbage type
+        for (int k = 0; k < garbage.size(); k++) {
+            string currentgarbage = garbage[k];
+            for (char kachara : currentgarbage) {
+                if (kachara == 'P') {
+                    pickP++;
+                    lastP = k;
+                }
+                if (kachara == 'G') {
+                    pickG++;
+                    lastG = k;
+                }
+                if (kachara == 'M') {
+                    pickM++;
+                    lastM = k;
+                }
+            }
+        }
+
+        // Calculate the travel time required for each garbage type
+        int travelTime = 0;
+        if (lastP != -1) {
+            for (int i = 0; i < lastP; i++) {
+                travelTime += travel[i];
+            }
+        }
+        if (lastG != -1) {
+            for (int i = 0; i < lastG; i++) {
+                travelTime += travel[i];
+            }
+        }
+        if (lastM != -1) {
+            for (int i = 0; i < lastM; i++) {
+                travelTime += travel[i];
+            }
+        }
+
+        totalTime = pickP + pickG + pickM + travelTime;
+        return totalTime;
+    }
+};
