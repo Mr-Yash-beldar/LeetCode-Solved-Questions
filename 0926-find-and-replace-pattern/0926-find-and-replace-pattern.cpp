77427f0b18f6398e18d00a0ad019683dd22602bf
Yashodip Beldar <106594839+Mr-Yash-beldar@users.noreply.github.com>
2024-08-31 11:07:15 +0530

    Time: 5 ms (32.63%), Space: 10.4 MB (73.26%) - LeetHub

--- 0926-find-and-replace-pattern/0926-find-and-replace-pattern.cpp
+++ 0926-find-and-replace-pattern/0926-find-and-replace-pattern.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
-    void createMapping(string& str) {
-        // mapping
-        char start = 'a';
-        char mapping[300] = {0};
-        for (char ch : str) {
-            if (mapping[ch] == 0) { // if mapping nto done hence 0
-                mapping[ch] = start;
-                start++;
-            }
+    void mapping(string& str){ //by reference to change orginal
+        char mapping[256]={0};
+        char start='a';
+        for(auto ch:str){
+            mapping[ch]=start;
+            start++;
         }
-
-        // updating words
-        for (int i = 0 ; i < str.size(); i++) {
-            char ch = str[i];     // taking each char of string
-            str[i] = mapping[ch]; // replace it by mapping of char
+        for(int i=0;i<str.length();i++){
+            char ch=str[i];
+            str[i]=mapping[ch]; //mapped original str
         }
-
-        // after this full array of words will be mapped with mapping char
     }
-    vector<string> findAndReplacePattern(vector<string>& words,
-                                         string pattern) {
-        vector<string> ans; // ans array
-        // find mapping of pattern as need to compare it;
-        createMapping(pattern);
-        for (string word : words) {
-            string tempString =
-                word; // as it will change after mapping but we need to store it
-                      // in ans hence taking temp
-                      createMapping(tempString); ///map string so that compare with mapped pattern
-            if(tempString==pattern) ans.push_back(word);  // as if mapping matched then follows pattern store original in ans
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+        vector<string> ans;
+        //create mappping for pattern
+        mapping(pattern);
+
+        //access each and create mapping for all
+        for(string s:words){
+            string tempstring=s; // use it for modify
+            mapping(tempstring); //mapped it
+            if(tempstring==pattern) {
+                //if matched that is it is valid ans as original should be return and which is s
+                ans.push_back(s); //store it
+            }
 
         }
         return ans;
