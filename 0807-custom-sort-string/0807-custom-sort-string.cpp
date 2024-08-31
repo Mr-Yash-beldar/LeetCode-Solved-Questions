82ee057c9a6f632b5bac47ef136ebf0047e661aa
Yashodip Beldar <106594839+Mr-Yash-beldar@users.noreply.github.com>
2024-08-31 10:16:25 +0530

    Time: 0 ms (100%), Space: 7.7 MB (72.65%) - LeetHub

--- 0807-custom-sort-string/0807-custom-sort-string.cpp
+++ 0807-custom-sort-string/0807-custom-sort-string.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    static string str;
+    static string str; //used global so that can be use in below array
     static bool compare(char char1, char char2) {
         return (str.find(char1) < str.find(char2));
     }
     string customSortString(string order, string s) {
-        str = order;
+        str = order; //defined it
         sort(s.begin(), s.end(), compare);
         return s;
     }
